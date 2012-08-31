ezsystems/ezpublish-kernel

commit 9b73bf4d5708d011f273da48185006758b0812b8

Implement EZP-61: Add permission use in SearchService
Also add a simple Integration test for this.

eZ/Publish/API/Repository/Tests/SearchServiceAuthorizationTest.php
@@ -1,0 +1,85 @@
+<?php
+/**
+ * File containing the SearchServiceAuthorizationTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Tests;
+
+use eZ\Publish\API\Repository\SearchService as SearchServiceInterface,
+    eZ\Publish\API\Repository\Values\Content\Query\Criterion,
+    eZ\Publish\API\Repository\Values\Content\Query,
+    eZ\Publish\API\Repository\Repository as RepositoryInterface,
+    eZ\Publish\API\Repository\Values\Content\Search\SearchResult,
+
+    eZ\Publish\Core\Base\Exceptions\NotFoundException,
+
+    eZ\Publish\SPI\Persistence\Handler;
+
+/**
+ * Test case for operations in the SearchService.
+ *
+ * @see eZ\Publish\API\Repository\SearchService
+ * @depends eZ\Publish\API\Repository\Tests\UserServiceTest::testLoadAnonymousUser
+ * @group integration
+ * @group authorization
+ */
+class SearchServiceAuthorizationTest extends BaseTest
+{
+    /**
+     * Test for the findContent() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\SearchService::findContent()
+     * @depends eZ\Publish\API\Repository\Tests\SearchServiceTest::testFindContent
+     */
+    public function testFindContentEmptyResult()
+    {
+        $repository = $this->getRepository();
+
+        /* BEGIN: Use Case */
+        $searchService = $repository->getSearchService();
+        $userService = $repository->getUserService();
+
+        // Set anonymous user
+        $repository->setCurrentUser( $userService->loadAnonymousUser() );
+
+        // This call will return an empty search result
+        $searchResult = $searchService->findContent( new Query( array( 'criterion' => new Criterion\LocationId( 5 ) ) ) );
+
+        self::assertEquals( 0, $searchResult->totalCount, "Search query on User location returned hits, should be 0" );
+        self::assertEmpty( $searchResult->searchHits );
+        /* END: Use Case */
+    }
+
+    /**
+     * Test for the findSingle() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\SearchService::findSingle()
+     * @expectedException eZ\Publish\API\Repository\Exceptions\NotFoundException
+     * @depends eZ\Publish\API\Repository\Tests\SearchServiceTest::testFindSingle
+     */
+    public function testFindSingleThrowsNotFoundException()
+    {
+        $repository = $this->getRepository();
+
+        /* BEGIN: Use Case */
+        $searchService = $repository->getSearchService();
+        $userService = $repository->getUserService();
+
+        // Set anonymous user
+        $repository->setCurrentUser( $userService->loadAnonymousUser() );
+
+        // This call will fail with a "NotFoundException" as user does not have access
+        $searchService->findSingle(
+            new Criterion\ContentId(
+                array( 4 )
+            )
+        );
+        /* END: Use Case */
+    }
+}

eZ/Publish/API/Repository/Tests/phpunit-legacy-solr.xml
@@ -22,6 +22,7 @@
                        all services here.
             -->
             <file>SearchServiceTest.php</file>
+            <file>SearchServiceAuthorizationTest.php</file>
         </testsuite>
     </testsuites>
     <filter>

eZ/Publish/API/Repository/Tests/phpunit-legacy.xml
@@ -38,6 +38,7 @@
             <file>URLWildcardServiceAuthorizationTest.php</file>
             <file>ObjectStateServiceAuthorizationTest.php</file>
             <file>SearchServiceTest.php</file>
+            <file>SearchServiceAuthorizationTest.php</file>
             <directory>FieldType/</directory>
         </testsuite>
     </testsuites>

eZ/Publish/Core/Repository/SearchService.php
@@ -13,6 +13,10 @@ use eZ\Publish\API\Repository\SearchService as SearchServiceInterface,
     eZ\Publish\API\Repository\Values\Content\Query\Criterion,
     eZ\Publish\API\Repository\Values\Content\Query,
     eZ\Publish\API\Repository\Repository as RepositoryInterface,
+    eZ\Publish\API\Repository\Values\Content\Search\SearchResult,
+
+    eZ\Publish\Core\Base\Exceptions\NotFoundException,
+
     eZ\Publish\SPI\Persistence\Handler;
 
 /**
@@ -65,7 +69,16 @@ class SearchService implements SearchServiceInterface
      */
     public function findContent( Query $query, array $fieldFilters = array(), $filterOnUserPermissions = true )
     {
-        // @TODO: Apply permission checks
+        $limitations = $this->repository->hasAccess( 'content', 'read' );
+        if ( $limitations === false )
+        {
+            return new SearchResult( array( 'time' => 0, 'totalCount' => 0 ) );
+        }
+        else if ( $filterOnUserPermissions && $limitations !== true )
+        {
+            $query->criterion = $this->addPermissionsCriterion( $query->criterion, $limitations );
+        }
+
         $result = $this->persistenceHandler->searchHandler()->findContent( $query, $fieldFilters );
         foreach ( $result->searchHits as $hit )
         {
@@ -93,7 +106,16 @@ class SearchService implements SearchServiceInterface
      */
     public function findSingle( Criterion $criterion, array $fieldFilters = array(), $filterOnUserPermissions = true )
     {
-        // @TODO: Apply permission checks
+        $limitations = $this->repository->hasAccess( 'content', 'read' );
+        if ( $limitations === false )
+        {
+            throw new NotFoundException( 'Content', '*' );
+        }
+        else if ( $filterOnUserPermissions && $limitations !== true )
+        {
+            $criterion = $this->addPermissionsCriterion( $criterion, $limitations );
+        }
+
         return $this->repository->getContentService()->buildContentDomainObject(
             $this->persistenceHandler->searchHandler()->findSingle( $criterion, $fieldFilters )
         );
@@ -110,5 +132,34 @@ class SearchService implements SearchServiceInterface
     public function suggest( $prefix, $fieldPaths = array(), $limit = 10, Criterion $filter = null )
     {
 
+    }
+
+    /**
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\Criterion $criterion
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Query\Criterion
+     */
+    private function addPermissionsCriterion( Criterion $criterion, array $limitations )
+    {
+        if ( empty( $limitations ) )
+            return $criterion;
+
+        $roleService = $this->repository->getRoleService();
+        foreach ( $limitations as $limitationSet )
+        {
+            /**
+             * @var \eZ\Publish\API\Repository\Values\User\Limitation $limitationValue
+             */
+            foreach ( $limitationSet as $limitationValue )
+            {
+                if ( !$criterion instanceof Criterion\LogicalAnd )
+                    $criterion = new Criterion\LogicalAnd( array( $criterion ) );
+
+                $type = $roleService->getLimitationType( $limitationValue->getIdentifier() );
+                $criterion->criteria[] = $type->getCriterion( $limitationValue, $this->repository );
+            }
+        }
+
+        return $criterion;
     }
 }

ezsystems/ezpublish-api

commit 41b584afc060786470c162760044c686072d357c

Fixed EZP-61 regression where Limitation Criteria did not use OR statment
Aligned implmentation with how it was done prevously in: https://github.com/ezsystems/ezp-next/blob/9e14c6b3133a2585c103376555849c5fcd8591d2/ezp/Content/Service.php#L487
Regression whas introduced in 9b73bf4d5708d011f273da48185006758b0812b8

Repository/Tests/SearchServiceAuthorizationTest.php
@@ -30,6 +30,33 @@ use eZ\Publish\API\Repository\SearchService as SearchServiceInterface,
 class SearchServiceAuthorizationTest extends BaseTest
 {
     /**
+     * Test for the findContent() method but with anonymous user.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\SearchService::findContent()
+     * @depends eZ\Publish\API\Repository\Tests\SearchServiceTest::testFindContent
+     */
+    public function testFindContent()
+    {
+        $repository = $this->getRepository();
+
+        /* BEGIN: Use Case */
+        $searchService = $repository->getSearchService();
+        $userService = $repository->getUserService();
+
+        // Set anonymous user
+        $repository->setCurrentUser( $userService->loadAnonymousUser() );
+
+        // Should return Content with location id: 2 as the anonymous user should have access to standard section
+        $searchResult = $searchService->findContent( new Query( array( 'criterion' => new Criterion\LocationId( 2 ) ) ) );
+        /* END: Use Case */
+
+        self::assertEquals( 1, $searchResult->totalCount, "Search query should return totalCount of 1" );
+        self::assertNotEmpty( $searchResult->searchHits, "\$searchResult->searchHits should not be empty" );
+        self::assertEquals( "Home", $searchResult->searchHits[0]->valueObject->contentInfo->name );
+    }
+
+    /**
      * Test for the findContent() method.
      *
      * @return void
@@ -49,10 +76,14 @@ class SearchServiceAuthorizationTest extends BaseTest
 
         // This call will return an empty search result
         $searchResult = $searchService->findContent( new Query( array( 'criterion' => new Criterion\LocationId( 5 ) ) ) );
-
-        self::assertEquals( 0, $searchResult->totalCount, "Search query on User location returned hits, should be 0" );
-        self::assertEmpty( $searchResult->searchHits );
         /* END: Use Case */
+
+        self::assertEmpty(
+            $searchResult->searchHits,
+            "Expected Not Found exception, got content with name: " .
+            (!empty($searchResult->searchHits) ? $searchResult->searchHits[0]->valueObject->contentInfo->name : '')
+        );
+        self::assertEquals( 0, $searchResult->totalCount, "Search query should return totalCount of 0" );
     }
 
     /**